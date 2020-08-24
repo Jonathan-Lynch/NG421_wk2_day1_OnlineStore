f3de3a99b673e1dbc57c283208af64134e4b2ddb
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-08-23 20:45:47 -0500

    dealt with product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { IProduct } from './model/iproduct';
+import { products } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  shoppingCart: IProduct[] = [];
+  products: IProduct[] = [];
+  constructor() {
+    this.products = products;
+  }
+
+  getProduct(): IProduct[] {
+    return this.products;
+  }
+
+  getShoppingCart(): IProduct[] {
+    return this.shoppingCart;
+  }
+
+  addToCart(product: IProduct): void {
+    this.shoppingCart.push(product);
+   }
 }
